a5be25d7de7846dfb285aa50dea7f2dc3fe0d438
sstutts316 <86498128+sstutts316@users.noreply.github.com>
2021-06-25 17:24:32 -0500

    Add files via upload

---
+++ Project3.cpp
@@ -0,0 +1,215 @@
+// Steven Stutts
+// CS - 210
+// 6/24/21
+
+#include <Python.h>
+#include <iostream>
+#include <Windows.h>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+/*
+Description:
+	To call this function, simply pass the function name in Python that you wish to call.
+Example:
+	callProcedure("printsomething");
+Output:
+	Python will print on the screen: Hello from python!
+Return:
+	None
+*/
+void CallProcedure(string pName)
+{
+	char* procname = new char[pName.length() + 1];
+	std::strcpy(procname, pName.c_str());
+
+	Py_Initialize();
+	PyObject* my_module = PyImport_ImportModule("PythonCode");
+	PyErr_Print();
+	PyObject* my_function = PyObject_GetAttrString(my_module, procname);
+	PyObject* my_result = PyObject_CallObject(my_function, NULL);
+	Py_Finalize();
+
+	delete[] procname;
+}
+
+/*
+Description:
+	To call this function, pass the name of the Python functino you wish to call and the string parameter you want to send
+Example:
+	int x = callIntFunc("PrintMe","Test");
+Output:
+	Python will print on the screen:
+		You sent me: Test
+Return:
+	100 is returned to the C++
+*/
+int callIntFunc(string proc, string param)
+{
+	char* procname = new char[proc.length() + 1];
+	std::strcpy(procname, proc.c_str());
+
+	char* paramval = new char[param.length() + 1];
+	std::strcpy(paramval, param.c_str());
+
+
+	PyObject* pName, * pModule, * pDict, * pFunc, * pValue = nullptr, * presult = nullptr;
+	// Initialize the Python Interpreter
+	Py_Initialize();
+	// Build the name object
+	pName = PyUnicode_FromString((char*)"PythonCode");
+	// Load the module object
+	pModule = PyImport_Import(pName);
+	// pDict is a borrowed reference 
+	pDict = PyModule_GetDict(pModule);
+	// pFunc is also a borrowed reference 
+	pFunc = PyDict_GetItemString(pDict, procname);
+	if (PyCallable_Check(pFunc))
+	{
+		pValue = Py_BuildValue("(z)", paramval);
+		PyErr_Print();
+		presult = PyObject_CallObject(pFunc, pValue);
+		PyErr_Print();
+	}
+	else
+	{
+		PyErr_Print();
+	}
+	//printf("Result is %d\n", _PyLong_AsInt(presult));
+	Py_DECREF(pValue);
+	// Clean up
+	Py_DECREF(pModule);
+	Py_DECREF(pName);
+	// Finish the Python Interpreter
+	Py_Finalize();
+
+	// clean 
+	delete[] procname;
+	delete[] paramval;
+
+
+	return _PyLong_AsInt(presult);
+}
+
+/*
+Description:
+	To call this function, pass the name of the Python functino you wish to call and the string parameter you want to send
+Example:
+	int x = callIntFunc("doublevalue",5);
+Return:
+	25 is returned to the C++
+*/
+int callIntFunc(string proc, int param)
+{
+	char* procname = new char[proc.length() + 1];
+	std::strcpy(procname, proc.c_str());
+
+	PyObject* pName, * pModule, * pDict, * pFunc, * pValue = nullptr, * presult = nullptr;
+	// Initialize the Python Interpreter
+	Py_Initialize();
+	// Build the name object
+	pName = PyUnicode_FromString((char*)"PythonCode");
+	// Load the module object
+	pModule = PyImport_Import(pName);
+	// pDict is a borrowed reference 
+	pDict = PyModule_GetDict(pModule);
+	// pFunc is also a borrowed reference 
+	pFunc = PyDict_GetItemString(pDict, procname);
+	if (PyCallable_Check(pFunc))
+	{
+		pValue = Py_BuildValue("(i)", param);
+		PyErr_Print();
+		presult = PyObject_CallObject(pFunc, pValue);
+		PyErr_Print();
+	}
+	else
+	{
+		PyErr_Print();
+	}
+	//printf("Result is %d\n", _PyLong_AsInt(presult));
+	Py_DECREF(pValue);
+	// Clean up
+	Py_DECREF(pModule);
+	Py_DECREF(pName);
+	// Finish the Python Interpreter
+	Py_Finalize();
+
+	// clean 
+	delete[] procname;
+
+	return _PyLong_AsInt(presult);
+}
+void displayMenu()
+{
+
+	cout << "Please choose menu option numbers 1 - 4. " << endl << endl;
+	cout << "1. Display a list of all items purchased in a given day along with the number of times each item was purchased" << endl;
+	cout << "2. Enter the name of an item to see how many times that item was purchased in a given day " << endl;
+	cout << "3. Display a text-based histogram listing all items purchased in a given day, along with a representation of the number of times each item was purchased " << endl;
+	cout << "4. Exit " << endl << endl;
+
+}
+
+void main()
+{
+	int choice = 0;
+	string item = "";
+	int frequency = 0;
+
+	do
+	{
+		displayMenu();
+		cin >> choice;
+
+		if (choice == 1)
+		{
+			CallProcedure("item_frequency_list");
+		}
+
+		else if (choice == 2)
+		{
+			cout << "Enter the name of the item: ";
+			cin >> item;
+			frequency = callIntFunc("get_frequency", item);
+
+			cout << item << " is repeated " << frequency << " times" << endl;
+		}
+
+		else if (choice == 3)
+		{
+			CallProcedure("read_write_file");
+			ifstream ifs("frequency.dat");
+			if (ifs.is_open())
+			{
+				while (!ifs.eof())
+				{
+					ifs >> item >> frequency;
+					cout << item << " ";
+
+					for (int i = 0; i < frequency; i++)
+					{
+						cout << "*";
+					}
+					cout << endl;
+				}
+
+				ifs.close();
+			}
+
+			else
+			{
+				cout << "Your file cannot be opened. " << endl;
+			}
+		}
+
+
+	} while (choice != 4);
+
+	//CallProcedure("printsomething");
+	//cout << callIntFunc("PrintMe", "House") << endl;
+	//cout << callIntFunc("SquareValue", 2);
+
+}
